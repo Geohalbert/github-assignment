0f4b1cdc962b7924890b286a66eae8a53ee04b2a
Hal Peters <geohalbert@gmail.com>
2020-12-12 13:52:33 -0600

    implementing react hooks

--- src/containers/CommitsContainer.js
+++ src/containers/CommitsContainer.js
@@ -1,18 +1,24 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text } from "react-native";
 
 export default function CommitsContainer() {
+  const [commits, setCommits] = useState();
+
   const fetchCommits = async () => {
     fetch("https://api.github.com/repos/geohalbert/github-assignment/commits")
       .then(response => response.json()) //Converting the response to a JSON object
-      .then(data =>
-        data.forEach(commit => {
-          let hash = commit.comments_url.slice(66, 106);
-          console.log(
-            `commit \n author: ${commit.commit.author.name} \n comment url: ${hash} \n message: ${commit.commit.message}`
-          );
-        })
-      )
+      .then(data => {
+        const commitArray = [];
+        data.forEach(resp => {
+          let user = resp.commit.author.name;
+          let hash = resp.comments_url.slice(66, 106);
+          let message = resp.commit.message;
+          let commit = { user, hash, message };
+          commitArray.push(commit);
+        });
+        setCommits(commitArray);
+        console.log(`commits: ${JSON.stringify(commits)}`);
+      })
       .catch(error => console.error(error));
   };
 
