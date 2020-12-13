48ea2e1ee2e034f0b04a6937179b311ebe79c8d2
Hal Peters <geohalbert@gmail.com>
2020-12-12 22:51:42 -0600

    added color scheme

---
+++ src/utils/colors.js
@@ -0,0 +1,10 @@
+export const Colors = {
+  primaryBackground: "#010409",
+  primaryBorder: "#30363d",
+  primaryTextColor: "#f0f6fc",
+  secondaryBackground: "#161b22",
+  secondaryBorder: "#21262d",
+  secondaryTextColor: "#8b949e",
+  specialTextColor: "#35AEEC",
+  separator: "#F4F4F4"
+};
