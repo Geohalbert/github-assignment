f2ce7dacce78b63f06e732832533dcc1713712c7
Hal Peters <geohalbert@gmail.com>
2020-12-13 05:43:16 -0600

    Header component created

---
+++ src/components/Header.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
+import { Colors } from "../utils/colors";
+
+export default function CommitsContainer() {
+  return (
+    <View style={styles.header}>
+      <View style={styles.title}>
+        <Image
+          style={styles.github}
+          source={require("../assets/github-icon.png")}
+        />
+        <Text style={styles.titleText}>Geohalbert/github-assignment</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  github: {
+    borderRadius: 30 / 2,
+    height: 30,
+    marginRight: 5,
+    width: 30
+  },
+  header: {
+    flexDirection: "column",
+    margin: 5
+  },
+  title: {
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "center"
+  },
+  titleText: {
+    color: Colors.primaryTextColor,
+    fontSize: 20,
+    fontWeight: "600"
+  }
+});
