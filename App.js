0606274c8bd28296e9abaad6aa5b8545024df5bf
Hal Peters <geohalbert@gmail.com>
2020-12-12 22:54:16 -0600

    app wrapped in safeview, update styling

--- App.js
+++ App.js
@@ -1,23 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { SafeAreaView, StyleSheet } from "react-native";
 import CommitsContainer from "./src/containers/CommitsContainer";
-
+import { Colors } from "./src/utils/colors";
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <CommitsContainer />
-      <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     alignItems: "center",
-    flex: 1,
-    justifyContent: "center",
-    paddingBottom: 40,
-    paddingTop: 40
+    backgroundColor: Colors.primaryBackground,
+    flex: 1
   }
 });
