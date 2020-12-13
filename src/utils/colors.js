760e001ac96bf1189632880ef0443b9a48f3c6b6
Hal Peters <geohalbert@gmail.com>
2020-12-13 05:35:34 -0600

    updated colors

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,10 +1,12 @@
 export const Colors = {
+  cardBackground: "#555555",
+  cardBody: "#ffffff",
   primaryBackground: "#010409",
   primaryBorder: "#30363d",
   primaryTextColor: "#f0f6fc",
   secondaryBackground: "#161b22",
   secondaryBorder: "#21262d",
   secondaryTextColor: "#8b949e",
-  specialTextColor: "#35AEEC",
-  separator: "#F4F4F4"
+  separator: "#F4F4F4",
+  specialTextColor: "#35AEEC"
 };
